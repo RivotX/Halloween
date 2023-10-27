b04212d225b87f1592e0cbc9affc46ee130fafed
RivotX <miguelrosso60@gmail.com>
2023-10-27 18:48:01 +0200

    habilidad empuje implementada

--- zzJuego/script.js
+++ zzJuego/script.js
@@ -2,8 +2,8 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');  //canvas context
 
-canvas.width = 900;
-canvas.height = 576;
+canvas.width = 1400;
+canvas.height = 800;
 
 c.fillRect(0, 0, canvas.width, canvas.height); //fillRect(x: number, y: number, w: number, h: number): pinta
 
@@ -28,7 +28,7 @@ class Sprite {
             width: 150,
             height: 30
         }
-
+        this.empuje = false;
         this.color = color;
         this.isAttacking;
         this.hp = hp;
@@ -124,7 +124,7 @@ daga.pintar();
 
 const arquero = new Sprite({
     position: {
-        x: 500,
+        x: 1350,
         y: 0
     },
     velocidad: {
@@ -167,9 +167,9 @@ function animate() { //esta funcion se esta llamando a si misma, es infinita has
 
     // Movilidad de daga
     if (keys.d.presionada == true && daga.UltimaTeclaHorizontal === "d") {
-        daga.velocidad.x = 2;
+        daga.velocidad.x = 5;
     } else if (keys.a.presionada == true && daga.UltimaTeclaHorizontal === "a") {
-        daga.velocidad.x = -2
+        daga.velocidad.x = -5
     }
     if (daga.position.y + daga.height + daga.velocidad.y >= canvas.height && keys.w.presionada == true && daga.UltimaTeclaVertical === "w") {
         daga.velocidad.y = -20;
@@ -180,9 +180,9 @@ function animate() { //esta funcion se esta llamando a si misma, es infinita has
 
     // Movilidad de arquero
     if (keys.ArrowRight.presionada == true && arquero.UltimaTeclaHorizontal === "ArrowRight") {
-        arquero.velocidad.x = 2;
+        arquero.velocidad.x = 3;
     } else if (keys.ArrowLeft.presionada == true && arquero.UltimaTeclaHorizontal === "ArrowLeft") {
-        arquero.velocidad.x = -2
+        arquero.velocidad.x = -3
     }
     if (arquero.position.y + arquero.height + arquero.velocidad.y >= canvas.height && keys.ArrowUp.presionada == true && arquero.UltimaTeclaVertical === "ArrowUp") {
         arquero.velocidad.y = -20;
@@ -263,11 +263,15 @@ const keys = {
     },
     ArrowDown: {
         presionada: false
+    },
+    k: {
+        presionada: false
     }
 }
 
 
 let ultimaVezDisparoFlecha = 0;
+
 
 
 window.addEventListener('keydown', function (event) {
@@ -294,7 +298,7 @@ window.addEventListener('keydown', function (event) {
         case "l":
             var tiempoActual = Date.now(); // Obtiene el tiempo actual
             if (arquero.position.x >= daga.position.x) {
-                if (tiempoActual - ultimaVezDisparoFlecha >= 800) {
+                if (tiempoActual - ultimaVezDisparoFlecha >= 700) {
                     ultimaVezDisparoFlecha = tiempoActual;  // Actualiza el tiempo del último disparo
                     console.log("disparo");
                     keys.l.presionada = true;
@@ -342,8 +346,6 @@ window.addEventListener('keydown', function (event) {
                     // Agrega la nueva flecha al array
                 }
             }
-
-
             break;
 
         case "ArrowRight":
@@ -363,6 +365,15 @@ window.addEventListener('keydown', function (event) {
             arquero.UltimaTeclaVertical = "ArrowDown";
             break;
 
+        case "k":
+            var tiempoActual = Date.now(); // Obtiene el tiempo actual
+
+            if (!arquero.empuje) {
+                arquero.empuje = true;
+                console.log("empuje");
+                keys.k.presionada = true;
+                daga.position = { x: 0, y: 700 };
+            }
     }
 })
 window.addEventListener('keyup', function (event) {
@@ -393,6 +404,9 @@ window.addEventListener('keyup', function (event) {
             break;
         case "ArrowDown":
             keys.ArrowDown.presionada = false;
+            break;
+        case "k":
+            keys.k.presionada = false;
             break;
     }
 })
