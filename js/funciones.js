75276cbf83ae375c869ebc7e9342e1909903979d
Your Name <you@example.com>
2023-10-28 14:07:10 +0200

    gg

---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+function recetasmostrar(){
+    const contenido= document.getElementsByClassName("contenido")[0];
+    const recetas=  document.getElementsByClassName("recetashalloween")[0];
+    contenido.style.display="none";
+    recetas.style.display="block";
+}
