d87da13d199d2bb2079c8a287a6d6f0159d720b6
Your Name <you@example.com>
2023-11-01 17:20:53 +0100

    gg

--- js/funciones.js
+++ js/funciones.js
@@ -4,3 +4,15 @@ function recetasmostrar(){
     contenido.style.display="none";
     recetas.style.display="block";
 }
+
+function entrar(){
+    const audio= document.getElementById("audio");
+    const elemento= document.getElementsByClassName("calavera")[0]
+elemento.style.animation="desvanecido 8s"
+audio.play();
+setTimeout(() => {
+    window.location.href="../Halloween/html/index.html";
+    
+}, 3000);
+
+}
